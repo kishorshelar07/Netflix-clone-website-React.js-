b8b308522316631d55bb973b3f4329ff6af61f4e
kishor <kishorshelar560@gmail.com>
2024-10-09 11:13:26 +0530

    some normal changes in this

--- src/Body.js
+++ src/Body.js
@@ -1,19 +1,22 @@
-
-import './Body.css';
+import "./Body.css";
 
 function Body() {
   return (
     <div className="Body">
       <div className="body-container">
-      <h1>Unlimited movies, TV shows and more</h1>
-      <h3>Starts at ₹149. Cancel at any time.</h3>
-      <p>Ready to watch? Enter your email to create or restart your membership.</p>
-      <div className="email-input">
-        <input type="email" placeholder="Email address" />
-        <button className="get-started-btn">Get Started</button>
+        <h1>Unlimited movies, TV shows and more</h1>
+        <h3>Starts at ₹149. Cancel at any time.</h3>
+        <p>
+          Ready to watch? Enter your email to create or restart your membership.
+        </p>
+        <div className="email-input">
+          <input type="email" placeholder="Email address" />
+          <button className="get-started-btn">Get Started</button>
+        </div>        
       </div>
+      <hr style={{color: "white",height:"5px"}} />
     </div>
-    </div>
+    
   );
 }
 
